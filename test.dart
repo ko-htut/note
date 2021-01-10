d93bfc05bc3f40a95932de5eedb6fb86740d9c2f
Ko Htut <kohtut@Kos-MacBook-Pro.local>
2021-01-10 15:22:25 +0630

    test

--- test.dart
+++ test.dart
@@ -1,8 +1,15 @@
-void main() {
-  abc();
-}
+import 'dart:html';
 
-void abc() {
-  String h = "Hello";
-  print(h);
+void main() {
+  List<int> mlist = [1, 2, 2, 2];
+  List<String> mlist2 = ["jlsdjfls", "sldkfsldkf"];
+
+  var mmmlist = ["sflkdfls", "sldkfjskdf"];
+  var mmmlist2 = [1, 2];
+
+  int a = 1;
+
+  var mset = {1, 2, 2, 2};
+  print(mset);
+  print(mlist);
 }
